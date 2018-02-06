464d58310dd233dab5f4774ee38c0a44c7c52ab0
Jose Guevara Arce <Jose Luis Guevara@jalasoft.local>
2018-02-05 22:10:46 -0400

    Up structure project git

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: __dirname + '/app/components/main.jsx',
+  output: {
+    path: __dirname + '/public/',
+    filename: "build.js",
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
